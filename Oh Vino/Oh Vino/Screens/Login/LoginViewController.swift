21639be43d9c3c64216cb283f3251cf729c1d48a
Kristsina Suvorava <krisvit1999@gmail.com>
2022-12-07 12:31:13 +0200

    UI for login screen was updated (+3 squashed commits) Squashed commits: [eae3f4a] Revert "Realm pakcage was added"
    This reverts commit 92b466a08902e0acd9ff81818a30685021a665ef.
    [92b466a] Realm pakcage was added
    [f961068] updated gitignore file

--- Oh Vino/Oh Vino/Screens/Login/LoginViewController.swift
+++ Oh Vino/Oh Vino/Screens/Login/LoginViewController.swift
@@ -12,13 +12,11 @@ class LoginViewController: SetUpKeyboardViewController {
 
     @IBOutlet weak var formsView: UIView!
     @IBOutlet weak var signInButton: UIButton!
-    @IBOutlet weak var appleContainerView: UIView!
     @IBOutlet weak var hidePasswordButton: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var rememberMeButton: UIButton!
 
-    private let signInWithAppleButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
     private let realmDataStore = RealmDataStore.shared
     private let userDefaults = UserDefaults.standard
     private var isButtonActive = false
@@ -27,12 +25,6 @@ class LoginViewController: SetUpKeyboardViewController {
         super.viewDidLoad()
         setUpformsView()
         setUpSignInButton()
-        setUpSignInWithAplleButton()
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        signInWithAppleButton.frame = CGRect(x: 0, y: 0, width: appleContainerView.frame.width, height: 50)
     }
 
     @IBAction func tappedGetOneButton(_ sender: UIButton) {
@@ -84,44 +76,5 @@ class LoginViewController: SetUpKeyboardViewController {
 
     private func setUpSignInButton() {
         signInButton.layer.cornerRadius = 10
-    }
-
-    private func setUpSignInWithAplleButton() {
-        appleContainerView.addSubview(signInWithAppleButton)
-        signInWithAppleButton.addTarget(self, action: #selector(didTapSignInWithApple), for: .touchUpInside)
-    }
-
-    @objc private func didTapSignInWithApple() {
-        let provider = ASAuthorizationAppleIDProvider()
-        let request = provider.createRequest()
-        request.requestedScopes = [.fullName, .email]
-
-        let controller = ASAuthorizationController(authorizationRequests: [request])
-        controller.presentationContextProvider = self
-        controller.performRequests()
-    }
-}
-
-extension LoginViewController: ASAuthorizationControllerDelegate{
-
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        print("error")
-    }
-
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        switch authorization.credential {
-        case let appleIDCredential as ASAuthorizationAppleIDCredential:
-            let username = appleIDCredential.user
-            let firstName = appleIDCredential.fullName?.givenName
-            let lastName = appleIDCredential.fullName?.familyName
-        default:
-            break
-        }
-    }
-}
-
-extension LoginViewController: ASAuthorizationControllerPresentationContextProviding{
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return view.window!
     }
 }
